0dfb2b89ea10ef4549a2143b6ff02087e40ffcc0
rCarto <timothee.giraud@cnrs.fr>
2022-03-15 15:39:57 +0100

    add gh logo + popup results

--- cityguess/server.R
+++ cityguess/server.R
@@ -1,23 +1,22 @@
-
-
-
 server <- function(input, output, session) {
+  
+  # data init
+  ncity=2
   vv <- sample(1:nrow(dataa), nrow(dataa), replace = FALSE)
   dataa <- dataa[vv,]
   ad <- paste0(dataa$name, ", ", dataa$ctry)
   ga <- substr(dataa$GAWC, 1,1)
-  
-  x <- (aggregate(ad, by = list(ga), head, 4))
+  x <- aggregate(ad, by = list(ga), head, 4)
   x <- unlist(x$x)
   x <- as.vector(x)
-  dataa <- dataa[ad %in% x, ][1:20,]
-
+  dataa <- dataa[ad %in% x, ][1:ncity,]
   rv <- reactiveValues(
     n = 1, 
     tot = 0,   
-    res = data.frame(city = paste0(dataa$name, ", ", dataa$ctry), dist=NA)
-    )
-
+    res = data.frame(City = paste0(dataa$name, ", ", dataa$ctry), Distance=NA)
+  )
+  
+  # Display map
   output$map <- renderLeaflet({
     leaflet(options = leafletOptions(minZoom = 2, maxZoom = 6)) %>%
       setView(lng = 0, lat = 40, zoom = 4) %>%
@@ -25,24 +24,23 @@ server <- function(input, output, session) {
                        options = providerTileOptions(noWrap = TRUE))
   })
   
+  # First city
   output$ville <- renderText({
     HTML(paste0("<b>", dataa[1,2], ", ", dataa[1, 1],
-                " </b><small><small>(", rv$n,"/20)</small></small>"))
+                " </b><small><small>(", rv$n,"/",ncity,")</small></small>"))
   })
+  
   ## Observe mouse clicks 
   observeEvent(input$map_click, {
-    
-    # restart session
-    if(rv$n == 21 ){
-      session$reload()
-      return()
-    }
-    # update city & clean map
+    # update city 
     output$ville <- renderText({ 
       HTML(paste0("<b>", dataa[rv$n,2], ", ", dataa[rv$n, 1], 
-                  " </b><small><small>(", rv$n,"/20)</small></small>"))
+                  " </b><small><small>(", rv$n,"/",ncity,")</small></small>"))
     })
+    # clean map
     leafletProxy('map') %>% removeMarker(c("1", "2")) 
+    
+    # update map
     click <- input$map_click
     clat <- click$lat
     clng <- click$lng
@@ -51,28 +49,53 @@ server <- function(input, output, session) {
     leafletProxy('map') %>%
       addMarkers(lng=dataa[rv$n, 4], lat=dataa[rv$n, 3], layerId = "2", 
                  icon =icons2) 
+    # compute distance
     a <- st_as_sf(data.frame(x = clng,y = clat), 
                   coords = c("x", "y"), crs = 4326)
     b <- st_as_sf(data.frame(x = dataa[rv$n, 4], y = dataa[rv$n, 3]), 
                   coords = c("x", "y"), crs = 4326)
     x <- round(as.numeric(st_distance(a, b)/1000), 0)
     rv$tot <- rv$tot + x
+    # Display distance
     showNotification(HTML(paste0("<b>", dataa[rv$n,2],": " , x, " km</b>")), 
                      type = "message")
-    rv$res[rv$n, "dist"] <- x
+    # store distance
+    rv$res[rv$n, "Distance"] <- x
+    row.names(rv$res) <- rv$res$City
     
+    # next city
     rv$n <- rv$n + 1
     
-    if(rv$n == 21 ){
+    if(rv$n == ncity + 1 ){
+      # Display
       output$x <- renderText({ 
-        output$tatab2 <- renderDT(
-          rv$res,options = list(lengthChange = FALSE, pageLength = 5,dom="tp", order = list(list(2, 'asc')))
-        )
-        output$ville <- renderText({ ""})
-        HTML(paste0("<h3>Total distance: ", rv$tot, 
-                    "km</h3>", 
-                    "Click on the map to start a new session"))
+        showModal(
+          modalDialog(
+            footer = actionButton("rst", "New Game", icon = icon("globe") ),
+            title = HTML(paste0("<b>Total distance: ", rv$tot,"km !</b>")),
+            size = "m", 
+            easyClose = FALSE,
+            fade = FALSE,
+            label ="resultats",
+            renderDT(datatable(
+              rv$res[,2,drop=F],
+              options = list(lengthChange = FALSE,
+                             pageLength = 5,dom="tp", 
+                             order = list(list(1, 'asc')))
+            ))
+          ))
+        output$ville <- renderText({""})
+        HTML(paste0(""))
       })
     }
+    
+    observeEvent(input$rst, {
+      session$reload()
+      return()
+    })
+    
+    
   })
 }
+
+
--- cityguess/ui.R
+++ cityguess/ui.R
@@ -1,46 +1,19 @@
-# 
-# ui <- fluidPage(
-#   titlePanel(title = "gUESS THE CITY LOC"),
-#   sidebarLayout(
-#     sidebarPanel(
-#       width = 3,
-#       p(),
-#       htmlOutput("ville"),
-#       p(),
-#       div(),
-#       htmlOutput("x")
-#     ),
-#     mainPanel(
-#       width=9,
-#       leafletOutput("map", height='90vh', width="70vw"),
-#     )
-#   )
-# )
-# 
-
-
-
-
 
 ui <- fluidPage(
   fluidRow(
     column(width = 12, 
            h4("Guess the city location"),
            htmlOutput("ville"),
-           DTOutput('tatab2', width = "50vw"),
            htmlOutput("x")
     ),
     column(width = 12, 
            leafletOutput("map", height = "75vh"),
     )
+  ),
+  fluidRow(
+    HTML("</br><br><a href='https://github.com/rCarto/geoguess/'><img src='github-brands.svg' width='15px'></a>")
+    # <br><br>https://github.com/rCarto/geoguess/")
+    
   )
 )
 
-# 
-# ui <- fluidPage(
-#   titlePanel(title = "Guess the city position"),
-#     mainPanel(
-#       leafletOutput("map", height='90vh', width="90vw"),
-#     
-#   )
-# )
